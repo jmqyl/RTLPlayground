65ca074f37a80e2a5a4aad5ca1c58feaf978a520
jmqyl <82797863+jmqyl@users.noreply.github.com>
2026-09-23 14:04:38 +0800

    Update machine.c

--- machine.c
+++ machine.c
@@ -604,9 +604,17 @@ __code const struct machine machine = {
 
 };
 
-#elif defined(MACHINE_PCB_K0402WS_V3) || defined(MACHINE_HI_K0402WS)  // Sold as a variety of devices, see doc/
+#elif defined(MACHINE_PCB_K0402WS_V3) || defined(MACHINE_HI_K0402WS) || defined(MACHINE_HYWS_SGT0204S) // Sold as a variety of devices, see doc/
 __code const struct machine machine = {
-	.machine_name = "PCB-K0402WS-V3.0",
+
+#if defined(MACHINE_HYWS_SGT0204S)
+    .machine_name = "HYWS-SGT0204S",
+#elif defined(MACHINE_PCB_K0402WS_V3)
+    .machine_name = "PCB-K0402WS-V3.0",
+#else
+    .machine_name = "HI-K0402WS",
+#endif
+
 	.isRTL8373 = 0,
 	.min_port = 3,
 	.max_port = 8,
@@ -1325,55 +1333,6 @@ __code const struct machine machine = {
     },
 
 };
-#elif defined MACHINE_HYWS_SGT0204S
-__code const struct machine machine = {
-	.machine_name = "HYWS_SGT0204S",
-	.isRTL8373 = 0,
-	.min_port = 3,
-	.max_port = 8,
-	.n_sfp = 2,
-	.log_to_phys_port = {0, 0, 0, 6, 1, 2, 3, 4, 5},
-	.phys_to_log_port = {4, 5, 6, 7, 8, 3, 0, 0, 0},
-	.is_sfp = {0, 0, 0, 2, 0, 0, 0, 0, 1},
-	
-	// Left SFP port
-	.sfp_port[0].pin_detect = GPIO38, 
-	.sfp_port[0].pin_los = GPIO_NA, 
-	.sfp_port[0].sds = 1, 
-	.sfp_port[0].i2c = I2CBUS( GPIO39_I2C_SDA4, GPIO40_I2C_SCL3_MDC1 ), 
-
-	// Right SFP port
-	.sfp_port[1].pin_detect = GPIO37,
-	.sfp_port[1].pin_los = GPIO_NA, 
-	.sfp_port[1].sds = 0, 
-	.sfp_port[1].i2c = I2CBUS( GPIO41_I2C_SDA3_MDIO1, GPIO40_I2C_SCL3_MDC1 ), 
-
-	.reset_pin = GPIO_NA,
-	.high_leds = { .mux = LED_28_SYS | LED_29, .enable = LED_27 | LED_28_SYS | LED_29 },
-	.port_led_set = { 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	/* Ports 1-4 RJ45 use set 0, port 5-6 SFP uses set 1 
-	 */
-	.led_sets = { 
-			{
-				LEDS_2G5 | LEDS_LINK | LEDS_10M | LEDS_ACT,
-				LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK | LEDS_ACT | LEDS_10G,
-				LEDS_1G | LEDS_LINK,
-				0
-			},
-			{  
-				LEDS_10G | LEDS_2G5 | LEDS_1G | LEDS_100M | LEDS_10M | LEDS_LINK,
-				LEDS_10G | LEDS_2G5 | LEDS_1G | LEDS_100M | LEDS_10M | LEDS_ACT,
-				0,
-				0
-			},
-	 },
-	.led_mux_custom = 1,
-	.led_mux = {
-				0x00, 0x01, 0x04, 0x05, 0x08, 0x09, 0x0c, 0x3f, 0x0d, 0x10,
-				0x11, 0x0e, 0x14, 0x11, 0x12, 0x15, 0x15, 0x16, 0x18, 0x19,
-				0x1a, 0x19, 0x1d, 0x1e, 0x1c, 0x1d, 0x20, 0x21
-		},
-	};
 #else
 	#error "Please select a machine type in machine.h"
 #endif
